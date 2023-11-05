14238d85b90d2892af376f801b746e66e69844ee
shza0399 <szha0399@uni.sydeny.edu.au>
2023-11-05 12:39:37 +1100

    i updated the jpg and html and sketch and style

--- sketch.js
+++ sketch.js
@@ -1,7 +1,69 @@
+let img;
+let rs = []
+let time;
+let speed = 10
+
+function preload() {
+  img = loadImage("artwork.jpg"); // Load the image
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight); // Set the canvas size to the window size
+  img.resize(windowWidth, windowHeight); // Resize the image to fill the canvas
+  noStroke();
+  img.loadPixels();
+
+  let gridSize = 10; // Size of each square in the grid
+
+  for (let x = 0; x < width; x += gridSize) {
+    for (let y = 0; y < height; y += gridSize) {
+      let index = (x + y * img.width) * 4;
+
+      let r = img.pixels[index];
+      let g = img.pixels[index + 1];
+      let b = img.pixels[index + 2];
+
+      // Draw a rectangle with a random width and height
+      let w = random(gridSize / 2, gridSize * 1.5);
+      let h = random(gridSize / 2, gridSize * 1.5);
+
+      rs.push({
+        x: x,
+        y: y,
+        w: w,
+        h: h,
+        r: r,
+        g: g,
+        b: b,
+        a: random(50, 255),
+        d: 1
+      })
+    }
+  }
+
+  time = millis()
 }
 
 function draw() {
-  background(220);
+  background(200)
+
+  noStroke();
+  for (let i = 0; i < rs.length; i++) {
+    fill(rs[i].r, rs[i].g, rs[i].b, rs[i].a);
+    rect(rs[i].x, rs[i].y, rs[i].w, rs[i].h);
+    rs[i].a = rs[i].a + (rs[i].d * speed);
+
+
+    if (rs[i].a >= 255) {
+      rs[i].d = -1
+    }
+    if (rs[i].a <= 50) {
+      rs[i].d = 1
+    }
+  }
+
+  if (millis() - time >= 5000) {
+    speed = int(random(10, 20))
+  }
+
 }
